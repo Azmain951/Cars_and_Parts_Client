9c9868e9e214847d7052d9b1a23fe38ad026eb5d
Afridi Azmain <Azmain951>
2022-05-22 13:59:48 +0600

    on order now button click redirecting to place order page with product _id

--- src/pages/Home/Product.js
+++ src/pages/Home/Product.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Product = ({ product }) => {
-    const { _id, name, img, description, min, stock, price } = product
+    const { _id, name, img, description, min, stock, price } = product;
+    const navigate = useNavigate();
+    const handleOrderNow = () => {
+        navigate(`/placeOrder/${_id}`)
+    }
     return (
         <div class="card shadow-xl">
             <figure class="px-5 pt-5">
@@ -16,7 +21,7 @@ const Product = ({ product }) => {
                 <p className='font-bold'>Price: ${price}<small> (per unit)</small></p>
             </div>
             <div class="w-full m-0 p-0">
-                <button class="btn btn-white w-full text-white rounded-t-none">Order Now</button>
+                <button onClick={handleOrderNow} class="btn btn-white w-full text-white rounded-t-none">Order Now</button>
             </div>
         </div>
     );
--- src/pages/Home/Products.js
+++ src/pages/Home/Products.js
@@ -8,11 +8,12 @@ const Products = () => {
     }, [])
     return (
         <div className='my-16'>
-            <h2 className='text-primary text-center text-5xl mb-10 uppercase'>Our Products</h2>
+            <h2 className='text-primary text-center text-4xl uppercase'>Our Products</h2>
+            <div class="divider px-10"></div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-10'>
 
                 {
-                    products.map(product => <Product
+                    products?.map(product => <Product
                         key={product._id}
                         product={product}
                     ></Product>)
