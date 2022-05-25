9fee3385a41383894a17d1c7bcb5ed9bc54ea99e
Afridi Azmain <Azmain951>
2022-05-25 22:02:39 +0600

    minor bug mixed

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-xs lg:max-w-6xl py-12">
-                    <h3 className='text-white uppercase text-2xl lg:text-5xl'>Car's Hub</h3>
+                    <h3 className='text-white uppercase text-2xl lg:text-5xl'>Cars and Parts</h3>
                     <h1 className="mb-5 text-4xl  lg:text-8xl uppercase"><span className='text-primary'>Committed</span><br />
                         To Super Quality</h1>
                     <p className="italic text-lg lg:text-2xl font-normal mb-8">We are a manufacturer of high quality products, reaching and even outperforming European standards, proven by international quality certificates.</p>
